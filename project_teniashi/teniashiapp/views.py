2be2b4d614b81782482710282ad9a335bf2c21c0
Sou Yonamine <souyonamine@SounoMacBook-Pro.local>
2022-06-25 20:58:57 +0900

    TENIASHIのWEBSITE作成、firstcommit

---
+++ project_teniashi/teniashiapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, DeleteView, ListView, DetailView
+
+class index(TemplateView):
+    template_name = 'teniashiapp/index.html'
+
+class honsen(TemplateView):
+    template_name = 'teniashiapp/honesen.html'
+
+class yosen(TemplateView):
+    template_name = 'teniashiapp/yosen.html'
+
+class projectlist(ListView):
+    template_name = 'teniashiapp/projectlist'
+
+class delete(DeleteView):
+    template_name = 'teniashiapp/delete.html'
+
+class createproject(CreateView):
+    template_name = 'teniashiapp/createproject.html'
+
+class detailproject(DetailView):
+    template_name = 'teniashiapp/detailproject.html'
+
+class ():
+    template_name = 'teniashiapp/'
+
+class ():
+    template_name = 'teniashiapp/'
